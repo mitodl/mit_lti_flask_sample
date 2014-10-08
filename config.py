e317a7abfb481d79d808169dc7e2074500f595e6
Ivica Ceraj <iceraj@gmail.com>
2014-10-08 12:36:45 -0400

    Added support for authentication

--- config.py
+++ config.py
@@ -1,2 +1,7 @@
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
+PYLTI_CONFIG = {
+    "consumers":{
+        "__consumer_key__":{ "secret":"__lti_secret__"}
+    }
+}
--- mit_lti_flask_sample.py
+++ mit_lti_flask_sample.py
@@ -1,4 +1,4 @@
-from flask import Flask, session
+from flask import Flask, session, request
 from flask import render_template
 from flask.ext.wtf import Form
 from wtforms import IntegerField, BooleanField
@@ -16,20 +16,24 @@ class AddFrom(Form):
     result = IntegerField('result')
     correct = BooleanField('correct')
 
-@app.route('/error')
-def error():
+def error(exception=dict()):
     return render_template('error.html')
 
 
-@app.route('/')
-@app.route('/index', methods=['GET'])
-@lti(request='any', error=error)
+@app.route('/is_up', methods=['GET'])
 def hello_world(lti=lti):
+    return render_template('up.html', lti=lti)
+
+
+@app.route('/',methods=['GET','POST'])
+@app.route('/index', methods=['GET'])
+@lti(request='any', error=error, app=app)
+def index(lti=lti):
     return render_template('index.html', lti=lti)
 
 
 @app.route('/add', methods=['GET'])
-@lti(request='session', error=error)
+@lti(request='session', error=error, app=app)
 def add_form(lti=lti):
     form = AddFrom()
     form.p1.data = randint(1, 100)
@@ -38,7 +42,7 @@ def add_form(lti=lti):
 
 
 @app.route('/grade', methods=['POST'])
-@lti(request='session', error=error)
+@lti(request='session', error=error, app=app)
 def grade(lti=lti):
     form = AddFrom()
     correct = ((form.p1.data + form.p2.data) == form.result.data)
@@ -46,6 +50,17 @@ def grade(lti=lti):
     lti.post_grade(1 if correct else 0)
     return render_template('grade.html', form=form)
 
-
 if __name__ == '__main__':
+    import logging
+    import sys
+
+    root = logging.getLogger()
+    root.setLevel(logging.DEBUG)
+
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(name)s - %(message)s')
+    ch.setFormatter(formatter)
+    root.addHandler(ch)
+
     app.run(debug=True)
