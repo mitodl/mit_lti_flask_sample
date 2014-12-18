15325331c799df83fe98746cc903807065627715
Carson Gee <x@carsongee.com>
2014-12-18 16:45:54 -0500

    Present a more 12 factor app style config.py file - Small style fixes - Documentation for using environment variables for configuration

--- config.py
+++ config.py
@@ -1,12 +1,35 @@
+"""
+Configuration file for flask sample application
+"""
+import os
+
+
 # enable CSRF
 WTF_CSRF_ENABLED = True
 
 # secret key for authentication
-SECRET_KEY = 'you-will-never-guess'
+SECRET_KEY = os.environ.get("FLASK_SECRET_KEY", "you-will-never-guess")
+
+# Sample client certificate example for 12 factor app
+# You would want to store your entire pem in an environment variable
+# with something like:
+# ```
+# export CONSUMER_KEY_CERT=$(cat <<EOF
+# < paste cert here>
+# EOF
+# )
+# ```
+
+CONSUMER_KEY_PEM_FILE = os.path.abspath('consumer_key.pem')
+with open(CONSUMER_KEY_PEM_FILE, 'w') as wfile:
+    wfile.write(os.environ.get('CONSUMER_KEY_CERT', ''))
 
 PYLTI_CONFIG = {
-    "consumers":{
-        "__consumer_key__":{ "secret":"__lti_secret__"}
+    "consumers": {
+        "__consumer_key__": {
+            "secret": os.environ.get("CONSUMER_KEY_SECRET", "__lti_secret__"),
+            "cert": CONSUMER_KEY_PEM_FILE
+        }
     }
 }
 
@@ -18,6 +41,6 @@ PYLTI_URL_FIX = {
         "https://localhost:8000/": "http://localhost:8000/"
     },
     "https://localhost/": {
-        "https://localhost/":"http://192.168.33.10/"
+        "https://localhost/": "http://192.168.33.10/"
     }
 }
